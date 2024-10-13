610c838f40a023b9edd912fd13ac66f6a3db17c2
msgna <msgna@MESGUN->
2024-10-13 16:46:00 +0200

    Add project files.

---
+++ andra saken/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace andra_saken
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("the forrest");
+            List<Animal> forrest = new List<Animal>();
+            forrest.Add(new Owl(3500, true, 94));
+            forrest.Add(new Horse(1200, false, 30));
+            forrest.Add(new Dolphin(100000, false, 3750));
+
+            bool day = true;
+
+            while (true)
+
+            {
+                string key = Console.ReadKey().Key.ToString();
+
+                Console.Clear();
+
+                // Console.WriteLine(key);
+                switch (key)
+                {
+                    case "D":
+                        day = true;
+                        Console.WriteLine("Nu är det dag!");
+                        break;
+                    case "N":
+                        day = false;
+                        Console.WriteLine("Nu är det natt!");
+                        break;
+
+                }
+
+                foreach (Animal animal in forrest)
+                {
+                    Console.Write("vikt: " + animal.Weight + " Noctural " + animal.Noctural);
+
+                    if (animal is Owl)
+                    {
+                        Console.WriteLine("wingspan:  " + ((Owl)animal).Wingspan);
+                    }
+                    if (animal is Horse)
+                    {
+                        Console.WriteLine("hay per day: " + ((Horse)animal).HayPerDay);
+                    }
+                    if (animal is Dolphin)
+                    {
+                        Dolphin dolphin = (Dolphin)animal;
+                        Console.WriteLine("distance per day: " + dolphin.DistancePerDay);
+                    }
+                }
+
+              
+
+
+            // List<Owl> OwlsInForrest = new List<Owl>();
+
+            // OwlsInForrest.Add(new Owl(3500, true, 94));
+            }
+
+        }
+    } 
+}
---
+++ andra saken/animal.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace andra_saken
+{
+    internal class Animal
+    {
+        public int Weight { get; set; }
+
+        public bool Noctural { get; set; }
+
+        public Animal(int weight, bool noctural)
+        {
+            Weight = weight;
+            Noctural = noctural;
+
+
+        }
+        public virtual void Move(bool day)
+        {
+            // inget händer ned den måste finnas
+        }
+
+    }
+
+    class Owl : Animal
+    {
+
+        
+        
+
+
+        public int Wingspan { get; set; }
+
+        public Owl(int weight, bool noctural, int wingspan) : base(weight, noctural)
+        {
+
+
+            Wingspan = wingspan;
+
+        }
+      
+       public override void Move(bool day)
+        {
+            if (Noctural == true && day == false)
+            {
+                Console.WriteLine("Ugglan flyger blad träden!");
+            }
+            else
+            {
+                Console.WriteLine("Ugglan sover i trädet");
+            }
+        }
+
+    }
+
+    class Horse : Animal
+    {
+        public int HayPerDay { get; set; } 
+
+         public Horse(int weight, bool nocturnal, int hayperDay) : base(weight, nocturnal)
+         {
+            HayPerDay = hayperDay;
+         }
+        public override void Move(bool day)
+        {
+            if (Noctural == false && day == true)
+            {
+                Console.WriteLine("Hästen galopperar.");
+            }
+            else
+            {
+                Console.WriteLine("Hästen sover under ett träd");
+            }
+        }
+    }
+
+    class Dolphin : Animal
+    {
+        public int DistancePerDay { get; set; }
+
+        public Dolphin(int weight, bool norcturnal, int distancePerDay)
+            : base(weight, norcturnal)
+        {
+            DistancePerDay = distancePerDay;
+
+        }
+ 
+
+        public virtual void Move(bool day)
+        {
+            // inget händer ned den måste finnas
+        }
+
+        
+    }    
+}
